b89e066275dc70b31706441e0ca9edbcbd675140
Tracey <tracy.yan2018@gmail.com>
2022-09-06 22:53:09 +1000

    add background img and content

--- script.js
+++ script.js
@@ -3,34 +3,63 @@ const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').Length
+const slidesLength = slideRight.querySelectorAll('div').length
 
 
-// console.log(slidesLength);
-
+// up/down button functionality
 let activeSlideIndex = 0
 
-slideLeft.style.top = `-${(slidesLength -1) * 100 }vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
-    // console.log(sliderHeight);
     if(direction === 'up') {
-        activeSlideIndex++;
+        activeSlideIndex++
         if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
         }
     } else if(direction === 'down') {
-        activeSlideIndex--;
+        activeSlideIndex--
         if(activeSlideIndex < 0) {
             activeSlideIndex = slidesLength - 1
         }
     }
 
-    slideRight.style.tranform = `trnslateY(-$(activeSlideIndex * sliderHeiht}px)`
-
-    slideLeft.style.tranform = `trnslateY($(activeSlideIndex * sliderHeiht}px)`
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
+
+// paly audio
+
+const playBtn = document.getElementsByClassName("btn btn-outline-light")[0];
+const con = document.getElementsByClassName("content")[0];
+const myAudio = document.getElementById("myAudio");
+playBtn.addEventListener('click', () => playAudio())
+
+const playAudio = (onOff) => {
+  if (onOff) {
+    myAudio.play();
+    onOff = false;
+  } else {
+    myAudio.pause();
+    onOff = true;
+    // this.className = "play";
+  }
+  const txt = document.getElementById("poem");
+  const Poem = txt.value; 
+const peomArr = Poem.split("<br>");
+let html = "";
+for (let i=0; i< peomArr.length; i++) {
+     const arr = peomArr[i].split("\n");
+     const text = arr[0];
+     if (text) {
+        html += `<p>+${text}+</p>`;
+     }
+     con.innerHTML = html;
+}
+}
+
+
